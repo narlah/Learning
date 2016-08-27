5cf87051d61ae238aa6a35197a34da7958cc2d1e
narlah <mitrandir@gmail.com>
2016-08-27 14:13:51 +0300

    Randomise music archive, gotta recreate the functionality on python i guess

---
+++ Projects/src/RandomizeMusic/MainRandomize.java
@@ -0,0 +1,12 @@
+package RandomizeMusic;
+
+import javax.swing.SwingUtilities;
+
+public class MainRandomize {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+            SwingRandomiseMusic ex = new SwingRandomiseMusic();
+            ex.setVisible(true);
+        });
+	}
+}
