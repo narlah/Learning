522d10b7d30fe3f1d83cf2e405312b2d6c313be6
narlah <mitrandir@gmail.com>
2016-11-20 05:12:40 +0200

    kth element of single linked list , three ways , one is optimised for performance in case a large list is given and long K

--- MainTestingArea/src/f7.java
+++ MainTestingArea/src/f7.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
+
+import org.json.JSONException;
+
 public class f7 {
 	// ***************************f6**********************************
 
