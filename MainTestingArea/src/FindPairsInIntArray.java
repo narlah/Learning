181f4a168675090543017ed5cee3818404b8dc75
narlah <mitrandir@gmail.com>
2017-08-04 01:42:59 +0300

    google interview practice session - find a pair in unsorted list (negatives allowed, all int) that is equal to a parameter.

---
+++ MainTestingArea/src/FindPairsInIntArray.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Function;
+
+public class FindPairsInIntArray {
+    public static int searchForSum = 8;
+
+    private static boolean checkForPairs(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length && i != j; j++) {
+                if (arr[i] + arr[j] == searchForSum) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkForPairsAdvanced(int[] arr) {
+        if (arr.length == 1) return false; //we need pairs
+        int i = 0, j = arr.length - 1, sum = 0;
+
+        while (i < j) {
+            sum = arr[i] + arr[j];
+            if (sum == searchForSum)
+                return true;
+            if (sum < searchForSum)
+                i++;
+            else
+                j--;
+
+        }
+        return false;
+    }
+
+    public static boolean checkForPairsHash(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        int current;
+        for (int anArr : arr) {
+            if (set.contains(anArr)) return true;
+            set.add(searchForSum - anArr);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        FindPairsInIntArray f = new FindPairsInIntArray();
+        List<int[]> initialData = new ArrayList<>();
+
+        initialData.add(new int[]{-1, -1, 2, 3, 9});
+        initialData.add(new int[]{1, 2, 4, 4});
+        initialData.add(new int[]{});
+        initialData.add(new int[]{1});
+        initialData.add(new int[]{0, 0});
+        initialData.add(new int[]{0, -1});
+        initialData.add(new int[]{0, 4, 0, 4});
+        check(FindPairsInIntArray::checkForPairs, initialData);
+        check(FindPairsInIntArray::checkForPairsAdvanced, initialData);
+        check(FindPairsInIntArray::checkForPairsHash, initialData);
+    }
+
+    private static void check(Function<int[],Boolean> f, List<int[]> initialData){
+        System.out.println(f); //still wondering how to get the name of the Function , but its freaking 2am , good night
+        for (int[] data : initialData) {
+            System.out.println(Arrays.toString(data) + " " + f.apply(data));
+        }
+        System.out.println("-----------------------");
+    }
+
+}
+
