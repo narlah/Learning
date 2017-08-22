90c3094d582b1507375b54095c32f393d76f1851
narlah <mitrandir@gmail.com>
2017-08-22 12:08:53 +0300

    secondFind?NthPiNumber

--- MainTestingArea/src/kosev/findPitoXNumber.java
+++ MainTestingArea/src/kosev/findPitoXNumber.java
@@ -1,17 +1,22 @@
 package kosev;
 
+import java.text.*;
+
 public class findPitoXNumber {
     public static void main(String[] args){
 
 	System.out.println("Pi will be calculated to the number you provide. Insert number : ");
     //    int a = System.in.readInt();
 	double pi = 3;
-
-	for(int i=0,j=2;i<=12;i++,j+=2){
+	DecimalFormat df = new DecimalFormat();
+	df.setMinimumFractionDigits(25);
+	for(int i=0,j=2;i<=10;i++,j+=2){
 	    int coef = j * (j+1) * (j+2);
-	    pi += Math.pow(-1, i%2)*4/(coef);
-
+	    double fullConf = Math.pow(-1, i%2)*4d/((double)coef);
+	    pi +=fullConf;
+	    
+	    System.out.println(pi + " " + fullConf);
 	}
-	System.out.println(pi);
+	System.out.println(df.format(pi));
     }
 }
