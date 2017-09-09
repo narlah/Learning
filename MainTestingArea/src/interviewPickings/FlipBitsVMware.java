72ee1a4e4b72a31c7064f69b01d0799fd55e890f
n <mitrandir@gmail.com>
2017-09-09 22:51:25 +0300

    Tvyrdica - alot of puzzels and tasks in intrviewBit

--- MainTestingArea/src/interviewPickings/FlipBitsVMware.java
+++ MainTestingArea/src/interviewPickings/FlipBitsVMware.java
@@ -1,6 +1,7 @@
 package interviewPickings;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FlipBitsVMware {
     public static void main(String[] args) {
@@ -8,24 +9,14 @@ public class FlipBitsVMware {
         System.out.println(a.flip("111110001111001"));
     }
 
-    private ArrayList<Integer> flip(String A) {
-        int[] a = new int[A.length()];
-        for (int i = 0; i < A.length(); i++) {
-            char c = A.charAt(i);
-            if (c == '1')
-                a[i] = 1;
-            else
-                a[i] = 0;
-        }
-        int maxDiff = 0;
-        int flipStartIndex = 0;
-        int flipEndIndex = 0;
+    public ArrayList<Integer> flip(String A) {
+        ArrayList<Character> aArr = new ArrayList<>();
+        A.chars().forEach(c -> aArr.add((char) c));
 
-        int currentDiff = 0;
-        int currentStart = 0;
+        int maxDiff = 0, flipStartIndex = 0, flipEndIndex = 0, currentDiff = 0, currentStart = 0;
 
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == 0) {
+        for (int i = 0; i < aArr.size(); i++) {
+            if (aArr.get(i) == '0') {
                 currentDiff -= 1;
             } else {
                 currentDiff += 1;
@@ -39,10 +30,7 @@ public class FlipBitsVMware {
                 currentStart = i + 1;
             }
         }
-        if (maxDiff == 0 && flipStartIndex == 0) return new ArrayList<>();
-        ArrayList<Integer> result = new ArrayList<>();
-        result.add(flipStartIndex + 1);
-        result.add(flipEndIndex + 1);
-        return result;
+        return (maxDiff == 0 && flipStartIndex == 0) ? new ArrayList<>() :
+                new ArrayList<>(Arrays.asList(flipStartIndex + 1, flipEndIndex + 1));
     }
 }
---
+++ MainTestingArea/src/interviewPickings/interviewBit/I100doors100monkeys.java
@@ -0,0 +1,47 @@
+package interviewPickings.interviewBit;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class I100doors100monkeys {
+
+
+    public static void main(String[] args) {
+
+        //link : https://rosettacode.org/wiki/100_doors#Java
+        boolean[] doors = new boolean[101];
+        for (int i = 1; i <= 100; i++) {
+            for (int j = i; j <= 100; j++) {
+                if(j % i == 0) doors[j] = !doors[j];
+            }
+        }
+        ArrayList open = new ArrayList();
+        for (int i = 1; i <= 100; i++) {
+            if (doors[i]) open.add(i);
+            System.out.printf("Door %d: %s%n", i, doors[i] ? "open" : "closed");
+        }
+        System.out.println(Arrays.toString(open.toArray()));
+//        char[] charA = new char[100];
+//        for (int i = 0; i < 100; i++) {
+//            charA[i] = '1';
+//        }
+//        int counter;
+//        for (int j = 1; j <= 98; j++) {
+//            counter = j-1;
+//            while (counter < 99) {
+//                char a = charA[counter];
+//                System.out.println(counter);
+//                charA[counter] = a == '0' ? '1' : '0';
+//                counter += j;
+//
+//            }
+//            System.out.println();
+//        }
+//        System.out.println(Arrays.toString(charA));
+//        for (int i = 0; i < 100; i++) {
+//            if (charA[i] == '0')
+//                System.out.print(i+1 + " ,  ");
+//
+//        }
+    }
+}
