40b40f784ec392b8810b9e8edaa9df8ac4787201
narlah <mitrandir@gmail.com>
2018-01-24 15:46:06 +0200

    Transfer to the other comp

--- MainTestingArea/src/f7.java
+++ MainTestingArea/src/f7.java
@@ -4,7 +4,10 @@ public class f7 {
     // ***************************f6**********************************
     public static void main(String[] args) {
         // ***************************f6**********************************
-
+        int[] arr = new int[]{1,100,20,50,40,30,31,66,74,145};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
         System.out.println("Hello World");
         int var1 = 1;
         int var2 = var1 + 1;
---
+++ MainTestingArea/src/interviewPickings/leetcode/SelfDividingNumbers_728.java
@@ -0,0 +1,39 @@
+package interviewPickings.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SelfDividingNumbers_728 {
+    public static void main(String[] args) {
+        SelfDividingNumbers_728 sss = new SelfDividingNumbers_728();
+        System.out.println(Arrays.toString(sss.selfDividingNumbers(1, 22).toArray()));
+    }
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> result = new ArrayList<>();
+        int counter10 = left % 10-1;
+        for (int i = left; i <= right; i++) {
+            counter10++;
+            if (counter10 == 10) {
+                counter10 = 0;
+                continue;
+            }
+            if (checkSelfDivide(i)) result.add(i);
+        }
+
+        return result;
+    }
+
+    public boolean checkSelfDivide(int number) {
+        int a = number;
+        while (a > 0) {
+            int n = a % 10;
+            if (n == 0 || number % n != 0) return false;
+            a = a / 10;
+        }
+        return true;
+
+    }
+
+}
