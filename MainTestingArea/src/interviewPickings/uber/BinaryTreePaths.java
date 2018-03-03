a841c986c830ad4e1cf44bc5c46415814a7ca230
n <mitrandir@gmail.com>
2018-03-04 00:30:29 +0200

    Found all paths in a tree ... sideways ... finally

--- MainTestingArea/src/interviewPickings/uber/BinaryTreePaths.java
+++ MainTestingArea/src/interviewPickings/uber/BinaryTreePaths.java
@@ -3,7 +3,6 @@ package interviewPickings.uber;
 import archive.BinaryTree;
 import archive.TreeNode;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,8 +19,6 @@ Interview Questions
 
 public class BinaryTreePaths {
     public static HashMap<TreeNode, ArrayList<ArrayList<TreeNode>>> allPathsPerNode = new HashMap<>();
-    public static HashMap<TreeNode, ArrayList<ArrayList<TreeNode>>> allPathsPerNodeLeft = new HashMap<>();
-    public static HashMap<TreeNode, ArrayList<ArrayList<TreeNode>>> allPathsPerNodeRight = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         int[] array = {2, 6, 11, 12, 1, 23, 19, 5, 45, 7, 99, 68, 8, 13, 20, 25, 14, 15, 16, 66, 31, 991};
@@ -31,41 +28,49 @@ public class BinaryTreePaths {
         TreeNode rootBalanced = BinaryTree.recBalance(array, 3);
         rootBalanced.printTree(System.out);
 
+        //finds all paths are sums to that number
         getPaths(new Params(rootBalanced, 20));
 
-        System.out.println("\nAllPaths:");
+
+        //Things get harder - we find all the paths going down through the root
         getAllPaths(new Params(rootBalanced, Integer.MAX_VALUE));
+        System.out.println("\nAllPaths DOWNSTREAM ----------------------------------------- :");
+
         for (TreeNode keyTreeNode : allPathsPerNode.keySet()) {
             System.out.println("\n->      " + keyTreeNode.getValue());
-            for (ArrayList<TreeNode> paths : allPathsPerNode.get(keyTreeNode)) {
-                System.out.println(Arrays.toString(paths.stream().map(TreeNode::getValue).collect(Collectors.toCollection(ArrayList::new)).toArray()));
+            ArrayList<ArrayList<TreeNode>> pathsForThatNode = allPathsPerNode.get(keyTreeNode);
+            for (ArrayList<TreeNode> path : pathsForThatNode) {
+                System.out.println(Arrays.toString(path.stream().map(TreeNode::getValue).collect(Collectors.toCollection(ArrayList::new)).toArray()));
             }
         }
 
-        System.out.println("\nSimple?");
-        getAllPathSimple(rootBalanced, new ArrayList<>());
-    }
+        System.out.println("\n All paths SIDEWAYS ----------------------------------------- : ");
+        //Now to the hard stuff - find all paths SIDEWAYS
+        for (TreeNode keyTreeNode : allPathsPerNode.keySet()) {
+            System.out.println("\n = " + keyTreeNode.getValue() + " = ");
+            ArrayList<ArrayList<TreeNode>> pathsForThatNode = allPathsPerNode.get(keyTreeNode);
 
-    public static void getAllPathSimple(TreeNode end, boolean goingLeft, int sum, ArrayList<TreeNode> pathSoFar) { //true - left, false - right
-        if (end == null) return;
-        pathSoFar.add(end);
-        sum += end.getValue();
-        if (goingLeft)
-        System.out.println(Arrays.deepToString(pathSoFar.toArray()));
-        else
+            //find the middle point - where the tho paths divide - the left portion and the right portion and then n^2
+            // them to get all the paths to the left combined with all the paths on the right
+            int indexOfChange = pathsForThatNode.size();
+            int valueOfChange = pathsForThatNode.get(0).get(0).getValue();
+            for (int i = 0; i < pathsForThatNode.size(); i++) {
+                if (pathsForThatNode.get(i).get(0).getValue() != valueOfChange) {
+                    indexOfChange = i;
+                    break;
+                }
+            }
 
-        if (end.left != null) {
-            getAllPathSimple(end.left, true, sum, pathSoFar);
-        }
-        if (end.right != null) {
-            getAllPathSimple(end.right, false, sum, pathSoFar);
-        }
-        int tempSum = sum - pathSoFar.get(0).getValue();
-        for (int i = 1; i <= pathSoFar.size() - 1; i++) {
+            if (indexOfChange < pathsForThatNode.size()) {
+                for (int i = 0; i < indexOfChange; i++) {
+                    for (int j = indexOfChange; j < pathsForThatNode.size(); j++) {
+                        System.out.print(Arrays.toString(pathsForThatNode.get(i).stream().map(TreeNode::getValue).collect(Collectors.toCollection(ArrayList::new)).toArray()) + "  -> " + keyTreeNode.getValue() + " -> ");
+                        System.out.println(Arrays.toString(pathsForThatNode.get(j).stream().map(TreeNode::getValue).collect(Collectors.toCollection(ArrayList::new)).toArray()));
+                    }
 
-            System.out.println(Arrays.toString(pathSoFar.subList(i, pathSoFar.size()).toArray()));
+                }
+            }
         }
-        pathSoFar.remove(pathSoFar.size() - 1);
     }
 
     public static void getAllPaths(Params param) {
