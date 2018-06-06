47087f6a0b52be328f774643fbdd4d705b6d51bc
narlah <mitrandir@gmail.com>
2018-06-06 16:54:09 +0300

    champaigne towers ... learned something tho , still gotta think about the 1000000000 case ...

--- src/interviewPickings/leetcode/ChampaigneTower.java
+++ src/interviewPickings/leetcode/ChampaigneTower.java
@@ -20,64 +20,89 @@ d = (1 - sq(8*a)) / 2
 public class ChampaigneTower {
     public static void main(String[] args) {
         ChampaigneTower ct = new ChampaigneTower();
-        //System.out.println(ct.champagneTower(1, 1, 1));
-        System.out.println(ct.champagneTower(6, 2, 0)); //5
-        //System.out.println(ct.champagneTower(1, 1, 1)); //5
-        //System.out.println(ct.champagneTower(2, 1, 1)); //5
-
+        System.out.println(ct.champagneTower(1000000000, 99, 99)); //.5
+        //wtf why 1000000000 is 99/99 is 0 ?!?!?!?!
     }
 
     public double champagneTower(int poured, int query_row, int query_glass) {
-        if (poured >= 5050)
-            return 1;
-        if (poured <= 0)
-            return 0;
-        int rowsFull = getFullRowsDependingOnPoured(poured);
-        int x = rowsFull;
-        if (query_row < rowsFull - 1) //inside the filled range
-            return 1;
-        if (query_row > rowsFull + 1) //outside of possible pouring
-            return 0;
-
-        int cupsFull = (rowsFull * (rowsFull + 1)) / 2;
-        int p = poured - cupsFull; //wine left
-        double k = (double) p / x;
-
-        if (k <= 1 && query_row == x) {
-            if (query_glass == 0 || query_glass == query_row)
-                return k / 2;
-            else
-                return k;
+        if (poured == 0) return 0;
+        double[] pyramid = new double[5050];
+        int[] rowIndexes = new int[100];
+        int rowIndex = 0;
+        for (int index = 0; index < 100; index++) {
+            rowIndex += index;
+            rowIndexes[index] = rowIndex;
         }
-        if (k > 1 && query_row == x + 1) {
-            double l = k - 1;
-            if (query_glass == 0 || query_glass == query_row)
-                return l / 2;
-            else
-                return l;
-        }
-        return 0;
-
-    }
-
-    private int getFullRowsDependingOnPoured(int poured) {
-        int left = 1, right = 100, middle;
-        while (true) {
-            middle = (left + right) / 2;
-            int tmp = (middle * (middle + 1)) / 2;
-            if (tmp == poured)
-                return middle;
-            else if (tmp < poured) {
-                left = middle + 1;
-                if (left > right)
-                    return middle;
-            } else {
-                right = middle - 1;
-                if (left > right) {
-                    return middle - 1;
+        pyramid[0] = poured;
+        for (int i = 0; i < 99; i++) {
+            boolean touched = false;
+            for (int j = 0; j <= i; j++) {
+                int currentCup = rowIndexes[i] + j;
+                int nextCupLeft = rowIndexes[i + 1] + j;
+                int nextCupRight = rowIndexes[i + 1] + j + 1;
+                double current = pyramid[currentCup];
+                if (current > 1) {
+                    touched = true;
+                    double divide = ((current - 1) / 2);
+                    pyramid[nextCupLeft] = pyramid[nextCupLeft] + divide;
+                    pyramid[nextCupRight] = pyramid[nextCupRight] + divide;
                 }
             }
-
+            if (!touched) break;
         }
+        return Math.min(1, pyramid[rowIndexes[query_row] + query_glass]);
+
     }
+//        if (poured >= 5050)
+//            return 1;
+//        if (poured <= 0)
+//            return 0;
+//        int rowsFull = getFullRowsDependingOnPoured(poured);
+//        int x = rowsFull;
+//        if (query_row < rowsFull - 1) //inside the filled range
+//            return 1;
+//        if (query_row > rowsFull + 1) //outside of possible pouring
+//            return 0;
+//
+//        int cupsFull = (rowsFull * (rowsFull + 1)) / 2;
+//        int p = poured - cupsFull; //wine left
+//        double k = (double) p / x;
+//
+//        if (k <= 1 && query_row == x) {
+//            if (query_glass == 0 || query_glass == query_row)
+//                return k / 2;
+//            else
+//                return k;
+//        }
+//        if (k > 1 && query_row == x + 1) {
+//            double l = k - 1;
+//            if (query_glass == 0 || query_glass == query_row)
+//                return l / 2;
+//            else
+//                return l;
+//        }
+//        return 0;
+//
+//    }
+//
+//    private int getFullRowsDependingOnPoured(int poured) {
+//        int left = 1, right = 100, middle;
+//        while (true) {
+//            middle = (left + right) / 2;
+//            int tmp = (middle * (middle + 1)) / 2;
+//            if (tmp == poured)
+//                return middle;
+//            else if (tmp < poured) {
+//                left = middle + 1;
+//                if (left > right)
+//                    return middle;
+//            } else {
+//                right = middle - 1;
+//                if (left > right) {
+//                    return middle - 1;
+//                }
+//            }
+//
+//        }
+//    }
 }
--- src/others/SendEmailToGmail.java
+++ src/others/SendEmailToGmail.java
@@ -14,30 +14,32 @@ public class SendEmailToGmail {
         String title = "zaglavie";
         String body = "nothing to see here , move on";
 
-        String port = "25";
+        //String port = "25";
         //String port = "587";
-        //String port = "465";
+        String port = "465";
 
         Properties props = System.getProperties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        //props.put("mail.smtp.port", port);
+        props.put("mail.smtp.port", port);
         //props.put("mail.smtp.socketFactory.port", port);
         //props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         //props.put("mail.smtp.socketFactory.fallback", "false");
 //        props.put("mail.transport.protocol", "smtps");
-        //props.put("mail.smtp.ssl.enable", "false");
-//        props.setProperty("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.setProperty("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
 //        props.put("mail.smtp.user", emailName);
 //        props.put("mail.smtp.EnableSSL.enable", "true");
-        props.put("mail.debug", "true");
+
 //        props.put("mail.password", emailPassword);
 //        props.put("mail.user", emailName);
 //        props.put("mail.from", emailName);
 //        props.put("mail.smtp.localhost", "codenjoy.com");
-//        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-//        props.put("mail.smtp.socketFactory.fallback", "false");
+
 //        props.put("mail.smtp.port", port);
 //        props.put("mail.smtp.socketFactory.port", port);
 
@@ -54,8 +56,9 @@ public class SendEmailToGmail {
         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
         message.setSubject(title);
         message.setContent(body, "text/html; charset=utf-8");
-        Transport transport = session.getTransport("smtp");
+        Transport transport = session.getTransport("smtps");
         transport.send(message);
+        transport.close();
         System.out.println("---- Done -----");
 
     }
