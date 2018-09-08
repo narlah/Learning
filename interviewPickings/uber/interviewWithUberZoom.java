8ded6d52283fbc6636a1753770f872130a1f8a8e
n <mitrandir@gmail.com>
2018-09-08 19:13:20 +0300

    DNA sequence , rolling hashes

--- interviewPickings/uber/interviewWithUberZoom.java
+++ interviewPickings/uber/interviewWithUberZoom.java
@@ -2,11 +2,12 @@ package interviewPickings.uber;
 //Our server is calling the external API using call_api that is doing the HTTP call.
 //The API provider charges us if we make more than 10 calls per second. How would you implement something that keeps us below the limit?
 
+import java.util.Random;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
 
 public class interviewWithUberZoom {
-    private static int LIMIT = 10;
+    private static final int LIMIT = 10;
     private Vector<Transaction> lastTransactions = new Vector<>();
 
     public static void main(String args[]) throws Exception {
@@ -14,44 +15,49 @@ public class interviewWithUberZoom {
     }
 
     private void balancer() throws InterruptedException {
-        Thread thread = new Thread(new Timer());
-        thread.start();
-
+//        Thread thread = new Thread(new Timer());
+//        thread.start();
+        Random r = new Random(50);
         for (int i = 0; i < 100; i++) {
             receive("Transaction with id : " + i, lastTransactions);
-            if (i < 50)
-                TimeUnit.MILLISECONDS.sleep(101);
+            if (i < 50) {
+                TimeUnit.MILLISECONDS.sleep(r.nextInt(500));
+            }
         }
     }
 
     private void receive(String s, Vector lastTransactions) {
         long currentMillisecond = System.currentTimeMillis();
 
-        if (lastTransactions.size() > LIMIT && (currentMillisecond - ((Transaction) lastTransactions.get(lastTransactions.size() - LIMIT)).time) < 1000) {
-            System.out.println("----- WARNING -----");
-            throw new RuntimeException("Limit reached, try again later.");
+        if (lastTransactions.size() > LIMIT) {
+            long difference = currentMillisecond - ((Transaction) lastTransactions.get(lastTransactions.size() - LIMIT)).time;
+            System.out.println(difference);
+            if (difference < 1000) {
+                System.out.println("----- WARNING -----");
+                throw new RuntimeException("Limit reached, try again later.");
+            }
         }
         System.out.println(s); //sending it
         lastTransactions.add(new Transaction(s, System.currentTimeMillis()));
     }
 
-    class Timer implements Runnable {
-        public void run() {
-            int currentTick = 0;
-            while (true) {
-                if (currentTick > 5) break;
-                try {
-                    Thread.sleep(1000);
-                    System.out.println("ticker : " + currentTick);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                currentTick++;
-                //trimArrayList should be implemented in case we run long list of course, but we can keep it as a log - lets say
-                //create empty array list while sending an old one to a new Thread that will save the content to a db or log file.
-            }
-        }
-    }
+//    class Timer implements Runnable {
+//        public void run() {
+//            int currentTick = 0;
+//            while (true) {
+//                if (currentTick > 5) break;
+//                try {
+//                    Thread.sleep(1000);
+//                    System.out.println("ticker : " + currentTick);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                currentTick++;
+//                //trimArrayList should be implemented in case we run long list of course, but we can keep it as a log - lets say
+//                //create empty array list while sending an old one to a new Thread that will save the content to a db or log file.
+//            }
+//        }
+//    }
 
     class Transaction {
         Transaction(String data, long time) {
